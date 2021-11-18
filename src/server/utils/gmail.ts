f73c44a51d6388594093f60d4b117453f4e366e1
sherpalden <sherpalden369@gmail.com>
2021-11-18 21:17:02 +0545

    send email collapse

--- src/server/utils/gmail.ts
+++ src/server/utils/gmail.ts
@@ -6,15 +6,15 @@ const sendEmail = async (emailOptions: IEmailOptions) => {
     try {
       const accessToken = oauth2Client.getAccessToken();
       const transport = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          type: "Oauth2",
-          user: "sherpalden369@gmail.com",
-          clientId: String(process.env.GOOGLE_CLIENT_ID),
-          clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
-          refreshToken: String(process.env.GOOGLE_REFRESH_TOKEN),
-          accessToken: String(accessToken),
-        },
+        // service: "gmail",
+        // auth: {
+        //   type: "Oauth2",
+        //   user: "sherpalden369@gmail.com",
+        //   clientId: String(process.env.GOOGLE_CLIENT_ID),
+        //   clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
+        //   refreshToken: String(process.env.GOOGLE_REFRESH_TOKEN),
+        //   accessToken: String(accessToken),
+        // },
       });
       const result = await transport.sendMail(emailOptions);
       resolve(result);
