fec816ddd7deae2b9eff877787ade5e7ab0fcf44
sherpalden <sherpalden369@gmail.com>
2022-09-09 07:23:50 +0545

    temp remove: server codes and admin pages

--- src/pages/admin/company/index.tsx
+++
@@ -1,8 +0,0 @@
-import AdminRoute from "../../../components/hoc/withAdminRoute";
-import PrivateRoute from "../../../components/hoc/withPrivateRoute";
-
-const Company = () => {
-  return <>Hello from Company page</>;
-};
-
-export default AdminRoute(PrivateRoute(Company));
--- src/pages/api/send-email/index.ts
+++
@@ -1,7 +0,0 @@
-import nc from "next-connect";
-import { onError, onNoMatch } from "../../../server/middleware/error";
-import contactCtrl from "../../../server/services/contact/contact.controller";
-
-const SendEmail = nc({ onError, onNoMatch }).post(contactCtrl.sendContactEmail);
-
-export default SendEmail;
